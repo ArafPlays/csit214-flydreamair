897c813c9abe77974828018dd6a462eccb95fafe
Araf Rahman <arafrahman563@gmail.com>
2025-10-20 03:27:28 +1100

    Create book.js

---
+++ js/book.js
@@ -0,0 +1,19 @@
+const searchForm = document.getElementById("searchForm")
+
+
+searchForm.addEventListener("submit",function(e){
+    e.preventDefault()
+    
+    const locationFrom = document.getElementById("locationFrom").value
+    const locationTo = document.getElementById("locationTo").value
+    const departDate = document.getElementById("departDate").value
+    const returnDate = document.getElementById("returnDate").value
+    const flightClass = document.getElementById("flightClass").value
+    const passengers = document.getElementById("passengers").value
+
+
+
+    localStorage.setItem("flightSearchDetails", JSON.stringify({"from":locationFrom,"to":locationTo,"depart":departDate,"return":returnDate,"fClass":flightClass,"passengers":passengers}))
+    
+    window.location.href="available-flights.html"
+})
